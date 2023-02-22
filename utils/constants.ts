ab1e859a499d8dff234deb4aea7306ca1eb33daf
Felix Maury <felix@pianity.com>
2023-02-22 16:42:10 +0100

    added types + mock data

---
+++ utils/constants.ts
@@ -0,0 +1,144 @@
+import { Song } from "@/utils/types";
+
+export const SONG_DATA: Song[] = [
+  {
+    id: "63e373caa01a9ffd4a4f84a8",
+    title: "Fuzzy Kittens (Vintage Italo DISKO Mix)",
+    artist: "Pied Piper",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/pied-piper/fuzzy-kittens-vintage-italo-disko-mix/track_full.mp3",
+  },
+  {
+    id: "63dbd7a9c20cb280ff2c732d",
+    title: "Layer Caked Records #55 - Daedalus",
+    artist: "Dan & Dan",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/dan-dan/layer-caked-records-55-daedalus/track_full.mp3",
+  },
+  {
+    id: "63dbf49f1ea1f9515b2b4e34",
+    title: "Dans La Peau [3T3#012]",
+    artist: "MA KU SU",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/ma-ku-su/dans-la-peau-3t3012/track_full.mp3",
+  },
+  {
+    id: "63d7fe23502b084badd8f597",
+    title: "Tanqueta",
+    artist: "Gaston Cabrera",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/gaston-cabrera/tanqueta/track_full.mp3",
+  },
+  {
+    id: "63e0eb855dbd31741d58d921",
+    title: "Twinkling Eyes [BR Pianity Collection #29]",
+    artist: "Distances",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/distances/twinkling-eyes-br-pianity-collection-29-/track_full.mp3",
+  },
+  {
+    id: "63dbd90fc20cb280ff2c7354",
+    title: "Layer Caked Records #54 - Flight Of Icarus",
+    artist: "Dan & Dan",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/dan-dan/layer-caked-records-54-flight-of-icarus/track_full.mp3",
+  },
+  {
+    id: "63ea65da554511fb5bcca844",
+    title: "Need You",
+    artist: "DmitriyRs",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/dmitriyrs/need-you/track_full.mp3",
+  },
+  {
+    id: "63ea666d554511fb5bcca84c",
+    title: "Fire Crackers",
+    artist: "DmitriyRs",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/dmitriyrs/fire-crackers/track_full.mp3",
+  },
+  {
+    id: "63dd2ef05dbd31741d58cee4",
+    title: "Layer Caked Records #52 - P Planet",
+    artist: "Utip",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/utip/layer-caked-records-52-p-planet_7/track_full.mp3",
+  },
+  {
+    id: "63dbf6321ea1f9515b2b4e53",
+    title: "Layer Caked Records #53 - Arcadia",
+    artist: "Utip",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/utip/layer-caked-records-53-arcadia/track_full.mp3",
+  },
+  {
+    id: "63da2a88502b084badd8fdf2",
+    title: "Step",
+    artist: "Flora Fona",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/flora-fona/step/track_full.mp3",
+  },
+  {
+    id: "63dbf3e791093bbbfa5bd5a7",
+    title: "Baby",
+    artist: "DmitriyRs",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/dmitriyrs/baby/track_full.mp3",
+  },
+  {
+    id: "63d3922eca59bbfdeb5e6550",
+    title: "El Mirador",
+    artist: "Schmidt (BR)",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/schmidt-br/el-mirador/track_full.mp3",
+  },
+  {
+    id: "63da632b3cad3d30278ab1f3",
+    title: "New Line Cinema [3T3#11]",
+    artist: "Nightdrive",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/nightdrive/new-line-cinema-3t311/track_full.mp3",
+  },
+  {
+    id: "63d79e99b75d17a97546f788",
+    title: "Mourning The Ghost (Before The Moonshine Mix)",
+    artist: "Hesanobody",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/hesanobody/mourning-the-ghost-before-the-moonshine-mix/track_full.mp3",
+  },
+  {
+    id: "63e378b8a01a9ffd4a4f84c1",
+    title: "Désert",
+    artist: "Emilie Simon",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/emilie-simon/desert/track_full.mp3",
+  },
+  {
+    id: "63d158701a7d3ca5ba47f14a",
+    title: "Justin Supercot",
+    artist: "Animebro",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/animebro/justin-supercot/track_full.mp3",
+  },
+  {
+    id: "63d158b51a7d3ca5ba47f15a",
+    title: "Green-Hill",
+    artist: "Animebro",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/animebro/greenhill/track_full.mp3",
+  },
+  {
+    id: "63d132f905546b5f7ce05f58",
+    title: "Hungry",
+    artist: "MR MOREK",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/choko/hungry-/track_full.mp3",
+  },
+  {
+    id: "63d159761b1fd63b111cce5b",
+    title: "Thoughts Of You",
+    artist: "Animebro",
+    audioURL:
+      "https://tracks.fra1.digitaloceanspaces.com/animebro/thoughts-of-you/track_full.mp3",
+  },
+];
